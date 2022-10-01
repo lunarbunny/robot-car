c054cab6a48755c88fbd29a359dfde4206b1488a
SwiftingDuster <tingwei1734@gmail.com>
2022-10-01 09:42:59 +0800

    Fix includes

--- msp432/main.c
+++ msp432/main.c
@@ -1,9 +1,5 @@
-#include "msp.h"
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
