ca63b7816158935e4c766ee68468aac88b958187
lunarbunny <tingwei1734@gmail.com>
2022-11-24 09:51:04 +0800

    Squashed commits from infrared
    commit 78313271f1464dc0ba5bcc4c0f5fdf2fd4790c37
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 17 17:48:55 2022 +0800
    
        Code Cleanup
    
    commit 94764a0b6295967d02605b64dbf28fe98aada3f9
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 17 16:59:17 2022 +0800
    
        bugfix sry 100% solved ty
    
    commit 52daff9e93e9690f86a8aa95d595fb5ebdd92b97
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 17 16:47:52 2022 +0800
    
        hotfix comparing
    
    commit b9f800990d2d5b83549a8256f75527886b055664
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 17 15:56:09 2022 +0800
    
        COMPARING WEEEEE
    
    commit 43e7389fbf1593a6bf6648a885e4e921178a2eb3
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Wed Nov 16 21:46:39 2022 +0800
    
        GIVE ME THE FILES
    
    commit 25674bd98bb6eba4777b979559fd3bb753885aa4
    Merge: efe6310 69f000f
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Wed Nov 16 21:46:23 2022 +0800
    
        Merge branch 'main' into InfraredBarcode
    
    commit efe63104711daad8d6683e98d52882686c3144b7
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 10 18:46:39 2022 +0800
    
        Reconfigured Thanks TingWei <3
    
        - Fix positional foresight of barcode decoding. Now works 100% of the time instead of luck LOL
    
    commit 3969a44806e5a976d68e2c8046d2afd067cf8235
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Nov 10 16:14:53 2022 +0800
    
        Added Character Comparison
    
        Barcode scanner now works but doesnt account for a lot of edge cases and still uses digital output
    
    commit fe99820af9049e4fb54854ac08c33035c5045697
    Merge: 9aa987a d347234
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Mon Oct 31 13:35:43 2022 +0800
    
        Merge branch 'main' into InfraredBarcode
    
    commit 9aa987a4cca429b5141c26c974e1dfd83929a2ac
    Author: kyethefluffyowl <friendsbacktoback@gmail.com>
    Date:   Thu Oct 27 01:46:39 2022 +0800
    
        The Timings Update
    
        Changes
        - Timings of scan are now captured and processed into long and short bars :)
        - Pls install PUTTY to see the printf statements
    
        Notes 📝
        - Still using D0 as input
        - Can only start scan EXACTLY on start bar of char. Not dynamic yet.
        - Returns raw result, no char table to compare to yet
        - Timings are /1000 cause it returns microseconds idk if we need that clarity

--- pico/InfraredSensor/infraredMain.c
+++ pico/InfraredSensor/infraredMain.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // Pico libraries
 #include "pico/types.h" // Timers
 #include "pico/stdlib.h"
@@ -28,15 +29,18 @@ int main () {
     int arrayVar = 0;           // Used in main loop to check 1 char scanned // To-do: Make it dynamic 
     int charStartEndCheck[9];  // Used in checking for * symbol
     int timeChanges[9];         // Stores the differences in timings
-    int charASCII[8];
 
-    // Char maps
-    int varcharStartEndStar[9];
+    // Character Logic Variables
+    int startReading = 0; 
+    int varcharStartEndStar[9]; // Used for only comparing * (starting/ ending characters)
+    int countStar = 0;
+    char varCharASCII = '~'; // Used for storing main data character
+    char finalString[22];   // 1 used for *, 1 to store ending char '/0' if not buffer overflow; 20 char storage
 
+    // CODE39 Char Arrays. 1 is LONG bar. 0 is SHORT.
     int charStartEndStar[9] = {0, 1, 0, 0, 1, 0, 1, 0, 0};
     int charSpace[9] = {0, 1, 1, 0, 0, 0, 1, 0, 0};
     int letterA[9] = {1, 0, 0, 0, 0, 1, 0, 0, 1};
-
     int letterB[9] = {0, 0, 1, 0, 0, 1, 0, 0, 1};
     int letterC[9] = {1, 0, 1, 0, 0, 1, 0, 0, 0};
     int letterD[9] = {0, 0, 0, 0, 1, 1, 0, 0, 1};
@@ -104,13 +108,13 @@ int main () {
             /* To sort and find out the diff between each time interval */
             for (int i = 0; i < 9; i++) {
                 timeChanges[i] = charStartEndCheck[i+1] - charStartEndCheck[i]; // Differences in each timing captured
-                printf("T%i in miliseconds corrected: %i (%i) \n", i, charStartEndCheck[i], timeChanges[i]);
+                // printf("T%i in miliseconds corrected: %i (%i) \n", i, charStartEndCheck[i], timeChanges[i]);
             }
 
-            printf("\n");
+            printf("New char detected\n");
             arrayVar = 0; // Reset var to start new char reading 
 
-            /* Find if its long or short, black or white ooO ooA */
+            /* Find if its long or short bars, black or white ooO ooA */
             int h1 = INT32_MIN; //TOP 1st, index of top 1st
             int h2 = INT32_MIN; //TOP 2nd, index of top 2nd
             int h3 = INT32_MIN; //TOP 3rd, index of top 3rd
@@ -138,43 +142,160 @@ int main () {
                     h3 = timeChanges[i];
                 }
             }
-            printf("TOP 1st is %i from T%i - T%i \n", h1, h1i, h1i + 1);
-            printf("TOP 2nd is %i from T%i - T%i \n", h2, h2i, h2i + 1);
-            printf("TOP 3rd is %i from T%i - T%i \n\n", h3, h3i, h3i + 1);
 
-
-            char black = 'B'; //black
-            char white = 'W'; //white
-
-            printf("0 short, 1 long, here are the results of the scan: \n");
+            /* Remembers the position of the long bars in respect to how the timings came in */
             for (int i = 0; i < 9; i++) {
                 if (i % 2) {
-                    if (i == h1i || i == h2i || i == h3i) {printf("T%i-T%i (%c1)\n", i, i+1, white); varcharStartEndStar[i] = 1;}
-                    else {printf("T%i-T%i (%c0) \n", i, i+1, white); varcharStartEndStar[i] = 0;}
+                    if (i == h1i || i == h2i || i == h3i) {varcharStartEndStar[i] = 1;}
+                    else { varcharStartEndStar[i] = 0;}
                 } 
                 else {
-                    if (i == h1i || i == h2i || i == h3i) {printf("T%i-T%i (%c1)\n", i, i+1, black); varcharStartEndStar[i] = 1;}
-                    else {printf("T%i-T%i (%c0) \n", i, i+1, black); varcharStartEndStar[i] = 0;}
+                    if (i == h1i || i == h2i || i == h3i) { varcharStartEndStar[i] = 1;}
+                    else { varcharStartEndStar[i] = 0;}
                 }
             }
 
-            // PRINTING CHARBINARY 
-            printf("\n ***** \t READING RESULT: \t *****\n");
+            // If character = * || Used to know when to deliver message - By looking at if got starting and ending character
+            if (compareArray(varcharStartEndStar, charStartEndStar)==0 && varCharASCII != '*')
+            {
+                varCharASCII = '*'; // Prevent looping in case of slow reading
+                if (countStar < 2) {
+                    countStar ++;
+                    // printf("=====ANNEYONG IDK KOREAN=====> %i", countStar);
+                }
+            }
 
-            if (compareArray(varcharStartEndStar, charStartEndStar)==0) {printf("* \n");}
-            if (compareArray(varcharStartEndStar, letterF)==0) {printf("F \n");}
-            if (compareArray(varcharStartEndStar, charSpace)==0) {printf("SPACE \n");} 
-            printf(" END READING RESULT: \n");
+            /* START CHARACTER SORTING */
+            // If first number in data section of array starts with 1 
+            // KRAHOE185MC3U.YW
+            if (varcharStartEndStar[0]){ // Currently using varcharStartEndStar, 
+                if (varcharStartEndStar[8]) 
+                {
+                    // U1AK
+                    if (compareArray(varcharStartEndStar, letterU)==0) {varCharASCII = 'U';}
+                    else if (compareArray(varcharStartEndStar, no1)==0) {varCharASCII = '1';}
+                    else if (compareArray(varcharStartEndStar, letterA)==0) {varCharASCII = 'A';}
+                    else {varCharASCII = 'K';}
+                } 
+                
+                else if (varcharStartEndStar[2])
+                {
+                    // W3CM
+                    if (compareArray(varcharStartEndStar, letterW)==0) {varCharASCII = 'W';}
+                    else if (compareArray(varcharStartEndStar, no3)==0) {varCharASCII = '3';}
+                    else if (compareArray(varcharStartEndStar, letterC)==0) {varCharASCII = 'C';}
+                    else {varCharASCII = 'M';}
+                }
+                else if (varcharStartEndStar[6])
+                {
+                    //.8HR
+                    if (compareArray(varcharStartEndStar, charPeriod)==0) {varCharASCII = '.';}
+                    else if (compareArray(varcharStartEndStar, no8)==0) {varCharASCII = '8';}
+                    else if (compareArray(varcharStartEndStar, letterH)==0) {varCharASCII = 'H';}
+                    else {varCharASCII = 'R';}
+                }
+                else 
+                {
+                    // OE5Y
+                    if (compareArray(varcharStartEndStar, letterO)==0) {varCharASCII = 'O';}
+                    else if (compareArray(varcharStartEndStar, letterE)==0) {varCharASCII = 'E';}
+                    else if (compareArray(varcharStartEndStar, no5)==0) {varCharASCII = '5';}
+                    else {varCharASCII = 'Y';}
+                }
+            } 
+            
+            // If first number in data section of array starts with 0
+            // QGNTDJ7%40LSBIPF296-+X*/$VZ SPACE
+            else if (!varcharStartEndStar[0]) {
+                // VX-2BL47DNGQ
+                if (varcharStartEndStar[8]) // Ending number 1
+                {
+                    if (varcharStartEndStar[2]) {
+                        //V2BL
+                        if (compareArray(varcharStartEndStar, letterV)==0) {varCharASCII = 'V';}
+                        else if (compareArray(varcharStartEndStar, no2)==0) {varCharASCII = '2';}
+                        else if (compareArray(varcharStartEndStar, letterB)==0) {varCharASCII = 'B';}
+                        else {varCharASCII = 'L';}
+                    }
+                    else if (varcharStartEndStar[6]) {
+                        //-7GQ
+                        if (compareArray(varcharStartEndStar, charDash)==0) {varCharASCII = '-';}
+                        else if (compareArray(varcharStartEndStar, no7)==0) {varCharASCII = '7';}
+                        else if (compareArray(varcharStartEndStar, letterG)==0) {varCharASCII = 'G';}
+                        else {varCharASCII = 'Q';}
+                    }
+                    else {
+                        //X4DN
+                        if (compareArray(varcharStartEndStar, letterX)==0) {varCharASCII = 'X';}
+                        else if (compareArray(varcharStartEndStar, no4)==0) {varCharASCII = '4';}
+                        else if (compareArray(varcharStartEndStar, letterD)==0) {varCharASCII = 'D';}
+                        else {varCharASCII = 'N';}
+                    }
+                    
+                }
+                else // Ending number 0
+                {
+                    // Z$/*+69FPIS0%JT SPACE <0>
+                    if (varcharStartEndStar[1]) 
+                    {
+                        // Z$/+ SPACE
+                        if (varcharStartEndStar[2]) {
+                            // Z SPACE
+                            if (compareArray(varcharStartEndStar, letterZ)==0) {varCharASCII = 'Z';}
+                            else {varCharASCII = ' ';}
+                        }
+                        else {
+                            // $/*+
+                            if (compareArray(varcharStartEndStar, charStartEndStar)==0) {varCharASCII = '*';}
+                            else if (compareArray(varcharStartEndStar, charDollar)==0) {varCharASCII = '$';}
+                            else if (compareArray(varcharStartEndStar, charBackslash)==0) {varCharASCII = '/';}
+                            else {varCharASCII = '+';}
+                        } 
+                    }
+                    else if (varcharStartEndStar[2])
+                    {
+                        // 69FPIS
+                        if (varcharStartEndStar[4]){
+                            //6FP
+                            if (compareArray(varcharStartEndStar, no6)==0) {varCharASCII = '6';}
+                            else if (compareArray(varcharStartEndStar, letterF)==0) {varCharASCII = 'F';}
+                            else {varCharASCII = 'P';}
+                        }
+                        else {
+                            //9IS
+                            if (compareArray(varcharStartEndStar, no9)==0) {varCharASCII = '9';}
+                            else if (compareArray(varcharStartEndStar, letterI)==0) {varCharASCII = 'I';}
+                            else {varCharASCII = 'S';}
+                        }
+                    }
+                    else {
+                        // 0%JT
+                        if (compareArray(varcharStartEndStar, no0)==0) {varCharASCII = '0';}
+                        else if (compareArray(varcharStartEndStar, charPercentage)==0) {varCharASCII = '%';}
+                        else if (compareArray(varcharStartEndStar, letterJ)==0) {varCharASCII = 'J';}
+                        else {varCharASCII = 'T';}
+                    }
+                }
+            }
+            /* END CHARACTER SORTING */
+
+            strncat(finalString, &varCharASCII, 1); // Concatenating the characters into var finalString
         }
 
-        
+        /* WHEN COUNTSTAR = 2, PRINT THE FINAL STRING HERE */
+        if (countStar >= 2) {
+            printf("\n\n YOUR FINAL READING IS ON THE FOLLOWING LINE: \n \033[1;31m %s \033[0m", finalString);
+            countStar = 0; // Reset count star
+            varCharASCII = '~'; // Reset variable
+            strcpy(finalString, ""); // Clear contents of string
+        }
 
         /** SCANNING OF B/W **/
         if (gpio_get(GPIO_PIN_INFRARED_IN1) == 1) // BLACK BAR
         {
             if (infraFlag == true) {
                 infraFlag = false; 
-                printf("T%i: %i\n", arrayVar, time_us_32()/1000); 
+                sleep_us(12); // Delay of at least 10 microseconds NEEDS to be here otherwise it wont work
                 charStartEndCheck[arrayVar] = time_us_32()/1000;
                 arrayVar += 1;
                 
@@ -185,7 +306,7 @@ int main () {
         else {
             if (infraFlag == false) {
                 infraFlag = true; 
-                printf("T%i: %i\n", arrayVar, time_us_32()/1000);
+                sleep_us(12); // Delay of at least 10 microseconds NEEDS to be here otherwise it wont work
                 charStartEndCheck[arrayVar] = time_us_32()/1000;
                 arrayVar += 1;
             }
